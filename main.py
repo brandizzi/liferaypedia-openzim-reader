46aa7511c4e2121365de3427f047d5a22541b775
Adam Victor Nazareth Brandizzi <adam@brandizzi.com.br>
2026-02-25 08:05:51 -0300

    ASk CLI tool to retrieve a specific number, optionally

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from liferaypedia_openzim_reader.zimreader import extract_zim_to_json
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 3:
-        print("Usage: python zim_to_json.py <input.zim> <output.json>")
+    if len(sys.argv) not in {3, 4}:
+        print("Usage: python zim_to_json.py <input.zim> <output.json> [<number_of_elements>]")
         sys.exit(1)
 
-    extract_zim_to_json(sys.argv[1], sys.argv[2], max_objects=40)
+    max_objects = int(sys.argv[3]) if len(sys.argv) > 3 else 40
+    extract_zim_to_json(sys.argv[1], sys.argv[2], max_objects)
